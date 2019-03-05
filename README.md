4fbe417f2c29de619f0d2cfc44c48b4d1afff2f5
Calvin Colton <calvincolton@gmail.com>
2019-03-04 19:37:38 -0800

    plinko updates

--- README.md
+++ README.md
@@ -19,3 +19,21 @@ To download this project using git, run the following command at your terminal:
 ```
 git clone https://github.com/StephenGrider/MLKits.git
 ```
+
+## Two common types of algorithms:
+
+### Classification
+The value of our labels belong to a discrete dataset.
+
+Examples:
+1) Based on how many hours a student studied for an exam, did they PASS or FAIL? --> PASS/FAIL
+2) Based on the content of this email, is it SPAM or NOT SPAM? --> SPAM/NOT SPAM
+3) Based on where a basketball player shot from, did he SCORE or NOT SCORE? --> SCORE/NOT SCORE
+(Each case has two discrete possibilities)
+
+### Regression
+The value of our labels belong to a continuous set.
+
+Examples:
+1) Based on the year, make, and model of a car, what is its value? --> $0 - $50k
+2) Based on an individual's daily caloric intake and minutes spent exercising, what is their weight? --> 80 - 400lb
--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,40 @@
+const outputs = [];
+const predictionPoint = 300;
+const k = 3;
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  // Ran every time a ball drops into a bucket
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+  // console.log(outputs);
 }
 
 function runAnalysis() {
-  // Write code here to analyze stuff
+  console.log(`Your ball will probably fall into bucket ${bucket}`);
 }
 
+const knn = (data, point) => {
+  return _.chain(data)
+    .map(row => [distance(row[0], point), row[3]])
+    .sortBy(row => row[0])
+    .slice(0, k)
+    .countBy(row => row[1])
+    .toPairs()
+    .sortBy(row => row[1])
+    .last()
+    .first()
+    .parseInt()
+    .value();
+};
+
+const distance = (pointA, pointB) => {
+  return Math.abs(pointA - pointB);
+};
+
+const splitDataset = (data, testCount) => {
+  const shuffled = _.shuffle(data);
+
+  const testSet = _.slice(shuffled, 0, testCount);
+  const trainingSet = _.slice(shuffled, testCount);
+
+  return [testSet, trainingSet];
+};
