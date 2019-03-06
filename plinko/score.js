1d2cad23fd7b548ed63d345056288ff66bac1bab
Calvin Colton <calvincolton@gmail.com>
2019-03-05 19:27:29 -0800

    knn with pythagorean min/max for multiple features, testing bounciness feature

--- plinko/score.js
+++ plinko/score.js
@@ -1,6 +1,6 @@
 const outputs = [];
 const predictionPoint = 300;
-const k = 3;
+// const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Ran every time a ball drops into a bucket
@@ -9,12 +9,39 @@ function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
 }
 
 function runAnalysis() {
-  console.log(`Your ball will probably fall into bucket ${bucket}`);
+  const testSetSize = 20;
+  const [testSet, trainingSet] = splitDataset(minMax(outputs, 3), testSetSize);
+  let numberCorrect = 0;
+
+  _.range(1, 15).forEach(k => {
+    const accuracy = _.chain(testSet)
+      .filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
+      .size()
+      .divide(testSetSize)
+      .value();
+    // the above, as a for loop:
+    // for (let i = 0; i < testSet.length; i++) {
+    //   const bucket = knn(trainingSet, testSet[i][0]);
+    //   console.log(bucket, testSet[i][3]);
+    //   if (bucket === testSet[i][3]) {
+    //     numberCorrect++;
+    //   }
+    // }
+    console.log('For k of', k, 'accuracy is:', accuracy);
+  });
 }
 
-const knn = (data, point) => {
+const knn = (data, point, k) => {
+  // point has 3 values!!!
   return _.chain(data)
-    .map(row => [distance(row[0], point), row[3]])
+    .map(row => {
+      // const features = _.initial(row);
+      // const label = _.last(row);
+      return [
+        distance(_.initial(row), point),
+        _.last(row)
+      ];
+    })
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -27,7 +54,15 @@ const knn = (data, point) => {
 };
 
 const distance = (pointA, pointB) => {
-  return Math.abs(pointA - pointB);
+  // just for distance:
+  // return Math.abs(pointA - pointB);
+
+  // example of pointA = [300, .5, 16]
+  return _.chain(pointA)
+    .zip(pointB)
+    .map(([a, b]) => (a - b) ** 2)
+    .sum()
+    .value() ** 0.5;
 };
 
 const splitDataset = (data, testCount) => {
@@ -37,4 +72,19 @@ const splitDataset = (data, testCount) => {
   const trainingSet = _.slice(shuffled, testCount);
 
   return [testSet, trainingSet];
+};
+
+const minMax = (data, featureCount) => {
+  const clonedData = _.cloneDeep(data);
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map(row => row[i]);
+    const min = _.min(column);
+    const max = _.max(column);
+
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+    }
+  }
+
+  return clonedData;
 };
