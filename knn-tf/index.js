25fb26acb894cede0423b343e36eb50bbc86cc7c
Calvin Colton <calvincolton@gmail.com>
2019-03-12 18:46:35 -0700

    knn with additional features

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -3,8 +3,13 @@ const tf = require('@tensorflow/tfjs');
 const loadCSV = require('./load-csv');
 
 function knn(features, labels, predictionPoint, k) {
+  const { mean, variance } = tf.moments(features, 0);
+  const scaledPrediction = predictionPoint.sub(mean).div(variance.pow(0.5));
+
   return features
-  .sub(predictionPoint)
+  .sub(mean)
+  .div(variance.pow(0.5))
+  .sub(scaledPrediction)
   .pow(2)
 	.sum(1)
 	.pow(0.5)
@@ -25,7 +30,7 @@ let { features, labels, testFeatures, testLabels } = loadCSV(
   {
     shuffle: true,
     splitTest: 10,
-    dataColumns: ['lat', 'long', 'sqft_lot'],
+    dataColumns: ['lat', 'long', 'sqft_lot', 'sqft_living'],
     labelColumns: ['price']
   }
 );
