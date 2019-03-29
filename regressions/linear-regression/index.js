2159b2fb327723e2c26d449f4360c7d5b52477fa
Calvin Colton <calvincolton@gmail.com>
2019-03-29 09:46:02 -0700

    restructure folders in preparation for LOGISTIC regressiongit add .

--- regressions/linear-regression/index.js
+++ regressions/linear-regression/index.js
@@ -1,10 +1,10 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
-const loadCSV = require('./load-csv');
+const loadCSV = require('../load-csv');
 const LinearRegression = require('./linear-regression');
 const plot = require('node-remote-plot');
 
-let { features, labels, testFeatures, testLabels } = loadCSV('./cars.csv', {
+let { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
   shuffle: true,
   splitTest: 50,
   dataColumns: ['horsepower', 'weight', 'displacement'],
@@ -31,10 +31,4 @@ console.log('R2 is:', r2);
 
 regression.predict([
   [120, 2, 380]
-]).print();
-
-regression.predict([
-  [300, 2, 400],
-  [130, 1.8, 200],
-  [500, 1.4, 300]
 ]).print();
