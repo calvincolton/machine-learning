6bec1b5b08d3ff407ef7bbab7af8a599abecc8b1
Calvin Colton <calvincolton@gmail.com>
2019-03-29 16:41:10 -0700

    updating test method on logistic regression class, adding sigmoid

--- regressions/logistic-regression/index.js
+++ regressions/logistic-regression/index.js
@@ -1,6 +1,7 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('../load-csv');
+const LogisticRegression = require('./logistic-regression');
 
 const { features, labels, testFeatures, testLabels } = loadCSV(
   '../data/cars.csv',
@@ -15,4 +16,17 @@ const { features, labels, testFeatures, testLabels } = loadCSV(
   }
 );
 
-console.log(labels);
+const regression = new LogisticRegression(features, labels, {
+  learningRate: 0.5,
+  iterations: 100,
+  batchSize: 50
+});
+
+regression.train();
+
+console.log(regression.test(testFeatures, testLabels));
+
+// regression.predict([
+//   [130, 307, 1.75],
+//   [88, 97, 1.065]
+// ]).print();
